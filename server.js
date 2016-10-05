495c772498a6a44ea222a87ece0b69990215af98
LSHWEB <lshweb@gmail.com>
2016-10-05 20:19:25 +0000

    altered server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-660!');
+});
+
+app.get('/outstanding-spider', function(request, response){
+    response.send('outstanding-spider');
 });
 
 app.listen(process.env.PORT || 4000);
